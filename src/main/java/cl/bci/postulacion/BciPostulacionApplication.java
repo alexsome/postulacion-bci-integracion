c6b0c64df86a42d4021b221c3939b7c43c35a979
Alex <alexandressotomendez@gmail.com>
2023-11-15 17:02:54 -0300

    create project

---
+++ src/main/java/cl/bci/postulacion/BciPostulacionApplication.java
@@ -0,0 +1,13 @@
+package cl.bci.postulacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BciPostulacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BciPostulacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/bci/postulacion/BciPostulacionApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.bci.postulacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BciPostulacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
